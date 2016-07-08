82c301fcb572001263d63310ff6aa5d6de7a08d8
Hannes Oberreiter <Virus-On-Mac@Hanness-MacBook-Pro.local>
2016-07-08 18:26:20 +0200

    fixed calibrate to work easier

--- calibrate/calibrate.ino
+++ calibrate/calibrate.ino
@@ -1,69 +1,55 @@
-// bienenforum.com ESP8266 (NodeMCU) Waage
-// 
-// OpenSoure - d.h. es kann jeder damit tun was er will ...
-// Alles für die Bienen :-)
-
+/*
+ * BIG thanks for the example to
+ * Example using the SparkFun HX711 breakout board with a scale
+ * By: Nathan Seidle
+ * SparkFun Electronics
+ * Date: November 19th, 2014
+*/
+/*
+ *  HX711.h library awesome bodge https://github.com/bogde/HX711
+ */
 
 #include "HX711.h"
 
-HX711 scale(12, 14); // // HX711-DT an Pin D6 (=12); HX711-SCK an Pin D5 (=14)
+#define DIGITALOUT  5     //HX711 DT
+#define CLOCK       6     //HX711 CKL
+float   SCALE =     690.64;  //200kg gauge weight, change this to your value
+long    OFFSET =    71372; //Weight of the scale build up
+
+HX711 scale(DIGITALOUT, CLOCK);
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + or a to increase calibration factor");
+  Serial.println("Press - or z to decrease calibration factor");
 
-scale.set_scale(); 
-scale.tare(); // Reset Waage auf 0
-
-Serial.begin(115200);
-Serial.println(F("Start HX711 Kalibrierung"));
-
+  scale.set_scale();
+  scale.set_offset(OFFSET);
+  
+  long zero_factor = scale.read_average(50); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
 }
 
 void loop() {
 
-Serial.println(F("Innerhalb von 20 Sekunden Referenzgewicht auf die Waage legen!"));
-delay(20000);  
-ESP.wdtDisable();
-  
-Serial.println(F("Kalbrierungswert:"));
-Serial.println(scale.get_units(20));
-ESP.wdtDisable();
+  scale.set_scale(SCALE);
+  Serial.print("Reading: ");
+  Serial.print(scale.get_units(10), 1);
+  Serial.print(" kg");
+  Serial.print(" calibration_factor: ");
+  Serial.print(SCALE);
+  Serial.println();
 
-Serial.println(F("Kalbrierungswert:"));
-Serial.println(scale.get_units(20));
-ESP.wdtDisable();
-
-Serial.println(F("Kalbrierungswert:"));
-Serial.println(scale.get_units(20));
-ESP.wdtDisable();
-
-Serial.println(F("Kalbrierungswert:"));
-Serial.println(scale.get_units(20));
-ESP.wdtDisable();
-
-Serial.println(F(""));
-Serial.println(F("Kalibrierungswert notieren. "));
-Serial.println(F("Kalibrierungswert durch Referenzgewicht dividieren, z.B Kalibrierungswert -213245,9 / Referenzgewicht 10Kg = Wert zum eintragen -21324,59"));
-Serial.println(F(""));
-Serial.println(F("Innerhalb von 5 sek. das Gewicht wieder entfernen."));
-delay(5000);
-ESP.wdtDisable();
-
-Serial.println(F("Jetzt noch den Offset Wert notieren, wird benötigt fuer die Nullstellung."));
-
-Serial.println(F("Offset:"));
-Serial.println(scale.read_average());
-ESP.wdtDisable();
-
-Serial.println(F("Offset:"));
-Serial.println(scale.read_average());
-ESP.wdtDisable();
-
-Serial.println(F("Offset:"));
-Serial.println(scale.read_average());
-ESP.wdtDisable();
-
-Serial.println(F("Offset:"));
-Serial.println(scale.read_average());
-ESP.wdtDisable();
-
+  if(Serial.available())
+  {
+    char temp = Serial.read();
+    if(temp == '+' || temp == 'a')
+      SCALE += 10;
+    else if(temp == '-' || temp == 'z')
+      SCALE -= 10;
+  }
 }
